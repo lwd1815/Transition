3b9f192500fba76d4737ebaca523ca1e4b34b9c9
lwd1815 <lwd1815@163.com>
2017-09-04 14:57:45 +0800

    自定义view绘图篇之区域

--- propertyanimation/src/main/java/com/example/propertyanimation/customview/fragment/FourFragment.java
+++ propertyanimation/src/main/java/com/example/propertyanimation/customview/fragment/FourFragment.java
@@ -5,13 +5,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import com.example.propertyanimation.R;
 import com.example.propertyanimation.base.BaseFragment;
+import com.example.propertyanimation.customview.view.FourView;
 
 /**
  * 创建者     李文东
  * 创建时间   2017/8/24 16:25
- * 描述	      ${TODO}
+ * 描述	      自定义view绘图篇之区域
  * 更新者     $Author$
  * 更新时间   $Date$
  * 更新描述   ${TODO}
@@ -35,6 +37,9 @@ public class FourFragment extends BaseFragment {
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     View view = View.inflate(getContext(), R.layout.fragment_child_four,null);
+    FrameLayout frameLayout = view.findViewById(R.id.four_child);
+    FourView fourView= new FourView(getContext());
+    frameLayout.addView(fourView);
     return view;
   }
 
---
+++ propertyanimation/src/main/java/com/example/propertyanimation/customview/view/FourView.java
@@ -0,0 +1,29 @@
+package com.example.propertyanimation.customview.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 创建者     李文东
+ * 创建时间   2017/9/4 14:52
+ * 描述	      ${TODO}
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   ${TODO}
+ */
+
+public class FourView extends View{
+  public FourView(Context context) {
+    super(context);
+  }
+
+  public FourView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public FourView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+}
