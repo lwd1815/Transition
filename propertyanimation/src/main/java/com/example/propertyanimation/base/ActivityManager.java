a353e589db72e04b4ba9e47442a1886f2f0edcd6
lwd1815 <lwd1815@163.com>
2017-09-18 10:05:53 +0800

    fix

--- propertyanimation/src/main/java/com/example/propertyanimation/base/ActivityManager.java
+++ propertyanimation/src/main/java/com/example/propertyanimation/base/ActivityManager.java
@@ -7,10 +7,10 @@ import java.util.Stack;
 /**
  * 创建者     李文东
  * 创建时间   2017/8/22 17:23
- * 描述	      ${TODO}
+ * 描述
  * 更新者     $Author$
  * 更新时间   $Date$
- * 更新描述   ${TODO}
+ * 更新描述
  */
 
 public class ActivityManager {
