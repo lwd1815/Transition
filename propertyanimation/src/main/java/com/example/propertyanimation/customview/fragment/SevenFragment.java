e543d878d505c24d93bc81ab73a9e971053db8c4
lwd1815 <lwd1815@163.com>
2017-12-09 12:07:05 +0800

    自定义view学习

--- propertyanimation/src/main/java/com/example/propertyanimation/customview/fragment/SevenFragment.java
+++ propertyanimation/src/main/java/com/example/propertyanimation/customview/fragment/SevenFragment.java
@@ -5,8 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import com.example.propertyanimation.R;
 import com.example.propertyanimation.base.BaseFragment;
+import com.example.propertyanimation.customview.view.SevenView;
 
 /**
  * 创建者     李文东
@@ -34,7 +36,10 @@ public class SevenFragment extends BaseFragment {
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    View view = View.inflate(getContext(), R.layout.fragment_child_seven,null);
+    View view = View.inflate(container.getContext(), R.layout.fragment_child_seven,null);
+    FrameLayout frameLayout = view.findViewById(R.id.seven_child);
+    SevenView view1=new SevenView(container.getContext());
+    frameLayout.addView(view1);
     return view;
   }
 
---
+++ propertyanimation/src/main/java/com/example/propertyanimation/customview/view/SevenView.java
@@ -0,0 +1,34 @@
+package com.example.propertyanimation.customview.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 创建者     李文东
+ * 创建时间   2017/12/9 11:59
+ * 描述
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述
+ */
+
+public class SevenView extends View {
+  public SevenView(Context context) {
+    super(context);
+  }
+
+  public SevenView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public SevenView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+  }
+}
