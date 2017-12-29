e6831c0e5d58f29808353f051baffbc563bbf23f
lwd1815 <lwd1815@163.com>
2017-12-29 13:00:41 +0800

    java设计模式之适配器

---
+++ propertyanimation/src/main/java/com/example/propertyanimation/javadesignpattern/signpatternadapter/AdapterSignPattern.java
@@ -0,0 +1,20 @@
+package com.example.propertyanimation.javadesignpattern.signpatternadapter;
+
+/**
+ * 创建者     李文东
+ * 创建时间   2017/12/29 12:28
+ * 描述       java设计模式之适配器模式
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述
+ */
+
+public class AdapterSignPattern {
+
+  /**
+   * 适配器模式,核心思想就是为了解决接口之间不兼容的问题而出现的,
+   * 是只要能让两个互不兼容的接口正常对接
+   *
+   * 适配器:将一个类的接口换成客户希望的另一个接口,适配器模式使得原来由于接口不兼容而不能一起工作的那些类可以一起工作
+   */
+}
